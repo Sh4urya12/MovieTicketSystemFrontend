cf3031c9225585ad1210741187bc7c1b73c56b57
Shaurya Samyak <shauryasamyak35@gmail.com>
2026-07-14 16:51:58 +0530

    File committed

--- src/Api/axiosClient.js
+++ src/Api/axiosClient.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
@@ -35,19 +37,28 @@ axiosClient.interceptors.response.use(
 
       originalRequest._retry = true;
       isRefreshing = true;
+
       const refreshToken = localStorage.getItem("refreshToken");
 
       try {
-        const { data } = await axios.post("http://localhost:8080/auth/refresh", { refreshToken });
+        const { data } = await axiosClient.post("/auth/refresh", {
+          refreshToken,
+        });
+
         localStorage.setItem("accessToken", data.accessToken);
         localStorage.setItem("refreshToken", data.refreshToken);
+
         processQueue(null, data.accessToken);
+
         originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+
         return axiosClient(originalRequest);
       } catch (refreshError) {
-        processQueue(refreshError, null);
+        processQueue(refreshError);
+
         localStorage.clear();
         window.location.href = "/login";
+
         return Promise.reject(refreshError);
       } finally {
         isRefreshing = false;
